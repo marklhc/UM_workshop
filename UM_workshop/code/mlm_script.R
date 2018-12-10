1e5d5e60f0f819d2f12f66732f738ea287e7c286
Hok Chio (Mark) Lai <marklhc@gmail.com>
2018-12-10 04:53:28 +0000

    Initial commit of workshop materials

---
+++ UM_workshop/code/mlm_script.R
@@ -0,0 +1,44 @@
+
+# Install Packages --------------------------------------------------------
+
+# This RStudio Cloud space already included the required packages for you to
+# follow this workshop. However, if you're to run the analyses demonstrated in
+# this workshop, you need the following packages:
+
+# install.packages(c("lme4", "tidyverse", "psych", "texreg", "gridExtra"))
+
+# For Bayesian analyses, you may also need to install `rstan`, which is a bit
+# complicated and you'll need to follow the steps for your OS in
+# https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started. You'll then 
+# need to install the `brms` package:
+
+# install.packages("brms")
+
+
+# Load the Data -----------------------------------------------------------
+
+
+
+# Data Exploration --------------------------------------------------------
+
+
+
+# (Unconditional) Random Intercept Model ----------------------------------
+
+
+
+# Including a Lv-2 Predictor ----------------------------------------------
+
+
+
+# Contextual Effects ------------------------------------------------------
+
+
+
+# Random Slopes/Coefficients ----------------------------------------------
+
+
+
+# Cross-Level Interaction -------------------------------------------------
+
+
